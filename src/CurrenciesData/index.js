2c2461fb3363d2b4b7510ef338340163b5408097
Krzysztof Gronski <krzysztof.gronski.pl@gmail.com>
2022-12-22 16:48:33 +0100

    check update requirement

--- src/CurrenciesData/index.js
+++ src/CurrenciesData/index.js
@@ -1,87 +1,63 @@
 import { useState, useEffect } from "react";
 
-let pending = false;
-let currencyData;
-
-const updateRequired = () => {
-    const updateDate = new Date(localStorage.getItem("updateDate"));
-    const currentDate = new Date();
-    if (pending) {
-        return false;
-    }
-    else if (localStorage.getItem("updateDate")
-        && localStorage.getItem("currenciesData")
-        && localStorage.getItem("currenciesSymbols")
-        && (updateDate.getFullYear() === currentDate.getFullYear()
-            && updateDate.getMonth() === currentDate.getMonth()
-            && updateDate.getDate() === currentDate.getDate())) {
-        return false;
-    }
-    return true;
-};
-
-const getCurrencyData = (res,currencySymbol) => {
+const getCurrencyData = async (currencySymbol) => {
     const API_URL = "https://api.exchangerate.host/";
     const requestParameters = `latest?base=${currencySymbol}`;
     
-    (async () => {
         try {
             const response = await fetch(`${API_URL}${requestParameters}`);
             if (!response.ok) {
                 throw new Error(response.statusText);
             }
-            currencyData = await response.json();
-            //await new Promise((res) => setTimeout(res, 5000));
-           // alert(currencyData);
-            res();
+            const currencyData = await response.json();
             return currencyData;
         }
         catch (error) {
             console.error("Fetching data error: ", error);
-            res();
             return null;
         }
-    })();
-    return currencyData;
+};
+
+const checkIsUpdateRequired = () => {
+	const lastUpdateDate = new Date(localStorage.getItem("updateDate")).getTime();
+	const currentDate = new Date().getTime();
+
+	const isLastUpdateDateExist = !!localStorage.getItem("updateDate");
+	const isCurrenciesDataExist = !!localStorage.getItem("currenciesData");
+	const isCurrenciesSymbolsExist = !!localStorage.getItem("currenciesSymbols");
+	const isOneOfLocalStorageItemNotExist = !isLastUpdateDateExist || !isCurrenciesDataExist || !isCurrenciesSymbolsExist;
+	const isTimeToUpdate = (currentDate - lastUpdateDate) > 24 * 60 * 60 * 1000;
+
+	return isOneOfLocalStorageItemNotExist || isTimeToUpdate;
 };
 
 export const useCurrenciesData = () => {
-    const [downloadStatus, setDownloadStatus] = useState("pending");
+    const [downloadStatus, setDownloadStatus] = useState("loading");
 
     useEffect(() => {
-        if (updateRequired()) {
-            pending = true;
+        if (checkIsUpdateRequired()) {
             (async () => {
-                    await new Promise((res)=>getCurrencyData(res,"PLN"));
-                    //await new Promise((res) => setTimeout(res, 5000));
-                    //alert(currencyData);
-                    
+                    let currencyData = await getCurrencyData("PLN");
                     if (currencyData) {
                         const currenciesSymbols = Object.keys(currencyData.rates);
                         localStorage.setItem("currenciesSymbols", JSON.stringify(currenciesSymbols));
 
-                        let newCurrenciesData = [];
-                        for (const currencySymbolIndex in currenciesSymbols) {
-                                await getCurrencyData(currenciesSymbols[currencySymbolIndex]);
-                                newCurrenciesData = [...newCurrenciesData, currencyData];
-                        };
+                        const newCurrenciesData = await Promise.all(currenciesSymbols.map((_, currencySymbolIndex) => 
+                        getCurrencyData(currenciesSymbols[currencySymbolIndex])));
+
                         localStorage.setItem("currenciesData", JSON.stringify(newCurrenciesData));
                         localStorage.setItem("updateDate", new Date().toISOString());
                         setDownloadStatus("resolved");
-                        alert("resolved");
-                        pending = false;
-
                     }
                     else {
                         setDownloadStatus("rejected");
                     }
             })();
         }
-        else if (!pending) {
+        else {
             setDownloadStatus("resolved");
         }
-        //return downloadStatus;
     }, []);
-    
+    return downloadStatus;
 
 };
